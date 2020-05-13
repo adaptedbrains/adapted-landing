68498fd5a8bab2fe85c37fe856ebe6268eb9872b
Vivek Ajage <vivek@logiqids.com>
2020-05-13 15:38:34 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ npm run prod:build-client
 echo "building server bundle"
 npm run prod:build-server
 echo "restart NurtureLabs pm2 server"
-pm2 delete mentions
+pm2 delete NurtureLabs
 pm2 start ecosystem.config.js
 
 echo "yo yo vivek baba deployment successful"
